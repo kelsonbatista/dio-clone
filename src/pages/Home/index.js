6d4ee4eaa7c38238e15e9b835d3faf4b5a65303f
Kelson <kelsonbatista@gmail.com>
2024-10-06 20:10:32 -0300

    Added Home and Login container

---
+++ src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { HomeContainer } from './styles'
+
+function Home() {
+  return (
+    <HomeContainer>
+      Home
+    </HomeContainer>
+  )
+}
+
+export default Home
---
+++ src/pages/Login/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { LoginContainer } from './styles'
+
+function Login() {
+  return (
+    <LoginContainer>
+      Login
+    </LoginContainer>
+  )
+}
+
+export default Login

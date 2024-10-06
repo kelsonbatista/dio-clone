ae40ae6fc36d7ed14dbaee5e4ed9f20388903a27
Kelson <kelsonbatista@gmail.com>
2024-10-06 19:50:24 -0300

    Added Header and Button components

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { HeaderContainer } from './styles'
+
+function Header() {
+  return (
+    <HeaderContainer>
+      Header
+    </HeaderContainer>
+  )
+}
+
+export default Header
